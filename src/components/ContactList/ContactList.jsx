0ee72f619e556ebe059869077c587253151c8c99
Oleksandr Yepishev <Alexandrdymka@gmail.com>
2021-10-27 13:11:19 +0300

    refactoring filter logic

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,21 +3,23 @@ import { useSelector } from 'react-redux';
 import { useMemo } from 'react';
 import useDebounce from '../../Hooks/debounce-hook';
 import { ContactItem } from './ContactItem';
-import { getFilter } from '../../redux/contacts/contacts-selectors';
+import {
+  getFilter,
+  getFilteredContacts,
+} from '../../redux/contacts/contacts-selectors';
 import { List } from './ContactList.styled';
 
 export const ContactList = ({ contacts }) => {
   const filter = useSelector(getFilter);
   const debouncedFilter = useDebounce(filter, 500);
 
-  const getFilteredContacts = useMemo(() => {
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(debouncedFilter.toLowerCase()),
-    );
+  const filteredContacts = useMemo(() => {
+    return getFilteredContacts(contacts, debouncedFilter);
   }, [contacts, debouncedFilter]);
+
   return (
     <List>
-      {getFilteredContacts.map(contact => (
+      {filteredContacts.map(contact => (
         <ContactItem key={contact.id} contact={contact} />
       ))}
     </List>
--- src/redux/contacts/contacts-selectors.js
+++ src/redux/contacts/contacts-selectors.js
@@ -1,1 +1,8 @@
 export const getFilter = state => state.contactsFilterReducer.filter;
+
+export const getFilteredContacts = (contacts, filter) => {
+
+  return  contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()),
+    );
+};
